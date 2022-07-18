88f84aebd2e6fdf8496234bb37c8a2f926821fc3
EdgardoDev <info@edgardopintoescalier.com>
2022-07-18 08:49:22 +0200

    Added small example of parametrized fixture test function

--- tests/test_example4.py
+++ tests/test_example4.py
@@ -34,7 +34,7 @@ from django.contrib.auth.models import User
 #     print(new_user_one.username)
 #     assert True
 
-def test_product(db, product_factory):
-    product = product_factory.create()
-    print(product.description)
-    assert True
+# def test_product(db, product_factory):
+#     product = product_factory.create()
+#     print(product.description)
+#     assert True
